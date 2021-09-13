c45f4b8459599640f4caf23a793f159d0470851f
Reggie <reginaldallonce@gmail.com>
2021-09-13 18:12:07 -0400

    another one

--- js/script.js
+++ js/script.js
@@ -6,10 +6,12 @@
 let player;
 let winner;
 let playeTwo;
-let reset
-let color;
+let reset;
+let gameBoard;
+let gameTotal = 0;
 
-let gameBoard =[
+function init() {
+gameBoard =[
     [null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null],
@@ -18,19 +20,23 @@ let gameBoard =[
     [null, null, null, null, null, null, null],
 
 ]
-
+}
 /*----- cached element references -----*/
-const board = document.querySelector('.board');
-const reset = document.querySelector('#reset');
+const board = document.getElementById('board');
+reset = document.querySelector('#reset');
 const startEl = document.querySelector('#start');
+const places = document.getElementsByClassName('places');
 
 
 
 /*----- event listeners -----*/
 
 board.addEventListener('click', function(e){
-    console.log(e.target.id)
+    console.log(e.target)
+    console.log(e.target.tagName)
 })
+
+
 
 
 /*----- functions -----*/
