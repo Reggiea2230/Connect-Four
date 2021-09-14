0dccd41d8c286b9da87870930c3ee296f000d420
Reggie <reginaldallonce@gmail.com>
2021-09-14 16:14:35 -0400

    loops added

--- js/script.js
+++ js/script.js
@@ -36,13 +36,14 @@ board.addEventListener('click', function(e){
     /*console.log(e.target.tagName)*/
 
 
-if (e.target.tagName === 'DIV'){
+if (e.target.tagName === 'DIV' && player){
     e.target.style.backgroundColor ='red';
-    console.log(e.target, "This works" )
-}else {
-    console.log(e.target, "did not work")
+}else if (e.target.tagName === 'DIV' && !player){
+    e.target.style.backgroundColor ='blue';
 }
-
+gameTotal++ 
+checkGameTurn()
+console.log(gameTotal, player)
 });
 
 
@@ -51,3 +52,17 @@ document.querySelector('button')
 	.addEventListener('click', reset);
 
 /*----- functions -----*/
+
+function checkGameTurn(){
+        return player = gameTotal % 2
+}
+// when increment our  
+function playerColor(){
+    if (player === true){
+
+    }
+}
+startEl 
+// counter % 2 returns true or false
+// 1 plaer 1 turn true if false player 2 turn
+// define function that checks if its player 1 or not then figure out where to call 
